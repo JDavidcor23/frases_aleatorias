2c0a3e12e84cf7a329e8ee7508f0659912d35ce3
JDavidcor23 <jorgedavid23diaz@gmail.com>
2022-01-21 15:26:56 -0500

    pagina terminada

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ function App() {
     const r = Math.random() * (max - min);
     return Math.floor(r);
   }
+  const link = "https://twitter.com/intent/tweet?text=" + frases[id].frase + "  --" + frases[id].autor;
   return (
     <div id="container">
       <div id="quote-box">
         <p id="text">{frases[id].frase}</p>
-        <p id="author">{frases[id].autor}</p>
-        <button id="new-quote" onClick={() => setId(randomNumber(1, 29))}>
-          Nueva frase
-        </button>
-        <a href="https://twitter.com" id="tweet-quote">
-          Twitter
-        </a>
+        <p id="autor">{frases[id].autor}</p>
+        <div id="container-link">
+          <a href={link} id="tweet-quote" target="_blank" rel="noreferrer">
+            <img
+              src="https://res.cloudinary.com/dhu6ga6hl/image/upload/v1642616220/j22blag4ezgpkl9awve1.png"
+              alt="Logo twitter"
+            />
+          </a>
+          <button id="new-quote" onClick={() => setId(randomNumber(1, 29))}>
+            Nueva frase
+          </button>
+        </div>
       </div>
     </div>
   );
